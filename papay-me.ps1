f718ed6c86d9480f3498cd5eeee2759c761a6e4d
arratres <arratres@gmail.com>
2022-07-24 20:09:04 +0200

    add primitive script

---
+++ papay-me.ps1
@@ -0,0 +1,13 @@
+Function Set-WallPaper($Value){
+    Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value $value
+    rundll32.exe user32.dll, UpdatePerUserSystemParameters
+}
+
+
+Invoke-WebRequest "https://github.com/arratres/papayed/blob/main/papay.jpg?raw=true" -OutFile "$env:TEMP\papay.jpg"
+Invoke-WebRequest "https://github.com/arratres/papayed/blob/main/barca.mp3?raw=true" -OutFile "$env:TEMP\barca.mp3"
+
+Set-Wallpaper -value "$env:TEMP\Papay.jpg"
+
+## opens barca
+Add-Type -AssemblyName presentationCore;$player = New-Object system.windows.media.mediaplayer;$player.Open("$env:TEMP\barca.mp3");$player.Play()
